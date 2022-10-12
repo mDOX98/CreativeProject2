4e982a774b522f472b1b82bf938de4fbe976d987
mdox98 <mdobsurn1@gmail.com>
2022-10-12 00:58:41 -0600

    Final Solution

--- mathScript.js
+++ mathScript.js
@@ -19,57 +19,35 @@ function getData(value) {
       updateOutput(json);
     });
 }
-function sortFunction(a,b){  
-    var dateA = new Date(a.date).getTime();
-    var dateB = new Date(b.date).getTime();
-    return dateA > dateB ? 1 : -1;  
-}; 
+function sortFunction(a, b) {
+  var dateA = new Date(a.date).getTime();
+  var dateB = new Date(b.date).getTime();
+  return dateA > dateB ? 1 : -1;
+}
 function updateOutput(value) {
   bigArray = value;
-  console.log(bigArray);
+
   var arr = bigArray.data;
   arr.sort(sortFunction);
-  console.log(arr)
-  var temp = '';
+
+  var temp = "";
   for (let i = 0; i < arr.length; i++) {
     iter = arr[i];
-    /*console.log(
-      iter.home_team.full_name +
-        " v " +
-        iter.visitor_team.full_name +
-        " || " +
-        iter.home_team_score +
-        "-" +
-        iter.visitor_team_score +
-        " " +
-        iter.date
-    );*/
-    /*temp += `<div class="row table-row">`;
-    temp += `<div class="col">${iter.date.slice(0,10)}</div>`;
-    temp += `<div class="col">${iter.home_team.full_name}</div>`;
-    temp += `<div class="col">${iter.visitor_team.full_name}</div>`;
-    temp += `<div class="col">${iter.home_team_score}-${iter.visitor_team_score}</div>`;
-    temp += `</div>`;*/
-
-
     temp += `<div class="card text-center smaller-card">`;
     temp += `<div class="card-body">`;
     temp += `<h5 class="card-title">${iter.home_team.full_name} v ${iter.visitor_team.full_name}</h5>`;
-    temp += `<p class="card-text text-muted">${iter.date.slice(5,10) +'-'+ iter.date.slice(0,4)}</p>`;
-    
+    temp += `<p class="card-text text-muted">${
+      iter.date.slice(5, 10) + "-" + iter.date.slice(0, 4)
+    }</p>`;
+
     temp += `</div>`;
     temp += `<div class="card-footer">`;
     temp += `Final Score: ${iter.home_team_score}-${iter.visitor_team_score}`;
     temp += `</div>`;
     temp += `</div>`;
-
-
-
   }
 
   tableContent.innerHTML = temp;
-
-  
 }
 
 getData();
